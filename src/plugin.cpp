a99959aca3a90633a4ab59ac70897d13064fb246
Daminikov <podymalov@vk.com>
2025-08-19 19:02:30 +0800

    1

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -12,5 +12,15 @@ SKSEPluginLoad(const SKSE::LoadInterface *skse) {
     SKSE::Init(skse);
     SetupLog();
     logger::info("Plugin loaded");
+
+    auto formId = 0x123456;
+    auto text = "Sweet Roll";
+    auto number = 69;
+
+    logger::trace("The text is \" {}\"", text);
+    logger::info("The form ID is: {} and as hex {:x}", formId, formId);
+
+    auto output = std::format("Number is {}", number);
+    logger::info("{}", output);
     return true;
 }
